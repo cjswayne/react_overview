3d8e7562a208ca9c7848eb1fa44b9b69c89df504
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-27 13:39:52 -0800

    finished

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from './store'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from './store'
 import App from './App.jsx'
-import './index.css'
+import './index.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider>
-      <App />
+      <Provider>
+        <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
